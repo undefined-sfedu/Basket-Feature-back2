b878765e735c28b0b9d9e416ca2495cec1dea6ef
ShinyParadise <shinodise@gmail.com>
2022-09-29 16:29:30 +0300

    added router

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from app.core import models
 from app.core.database import engine
 from fastapi import FastAPI
 from app.api.endpoints.users import user_router
+from app.api.endpoints.teams import teams_router
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(teams_router)
