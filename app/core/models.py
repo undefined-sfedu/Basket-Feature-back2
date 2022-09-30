a25c1be34702fcc682c9dc021434a739f1f0c1cf
ShinyParadise <shinodise@gmail.com>
2022-09-30 21:00:38 +0300

    player model

--- app/core/models.py
+++ app/core/models.py
@@ -24,3 +24,17 @@ class Team(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
 
     user = relationship("User", back_populates="teams")
+    players = relationship("Player", back_populates="team")
+
+
+class Player(Base):
+    __tablename__ = "players"
+
+    id = Column(Integer, primary_key=True, index=True)
+    number = Column(Integer, nullable=False, default=1)
+    first_name = Column(String)
+    last_name = Column(String)
+    middle_name = Column(String)
+    team_id = Column(Integer, ForeignKey("teams.id"))
+
+    team = relationship("Team", back_populates="players")
