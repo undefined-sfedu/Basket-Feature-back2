b59c101a468bcb265a9292270fc124f646a57612
ShinyParadise <shinodise@gmail.com>
2022-09-30 21:01:53 +0300

    player methods

--- app/api/endpoints/teams.py
+++ app/api/endpoints/teams.py
@@ -4,15 +4,19 @@ from sqlalchemy.orm import Session
 from app.core.dependencies import get_db
 
 
-teams_router = APIRouter(prefix="/teams", tags=["teams"])
+team_router = APIRouter(prefix="/teams", tags=["teams"])
 
 
-@teams_router.get("/get_all", response_model=list[schemas.Team])
+@team_router.get("/get_all", response_model=list[schemas.Team])
 def get_all_teams(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    teams = crud.get_teams(db, skip=skip, limit=limit)
-    return teams
+    return crud.get_teams(db, skip=skip, limit=limit)
 
 
-@teams_router.get("/{user_id}", response_model=list[schemas.Team])
-def get_user_teams(user_id: int, db: Session = Depends(get_db)):
-    return crud.get_user(db, user_id).teams
+@team_router.get("/{team_id}", response_model=list[schemas.Player])
+def get_team_players(team_id: int, db: Session = Depends(get_db)):
+    return crud.get_team(db, team_id=team_id).players
+
+
+@team_router.post("/{team_id}/players", response_model=schemas.Player)
+def create_player_for_team(team_id: int, player: schemas.PlayerCreate, db: Session = Depends(get_db)):
+    return crud.create_team_player(db, player, team_id)
--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from app.core import models
 from app.core.database import engine
 from fastapi import FastAPI
 from app.api.endpoints.users import user_router
-from app.api.endpoints.teams import teams_router
+from app.api.endpoints.teams import team_router
+from app.api.endpoints.players import player_router
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(user_router)
-app.include_router(teams_router)
+app.include_router(team_router)
+app.include_router(player_router)
