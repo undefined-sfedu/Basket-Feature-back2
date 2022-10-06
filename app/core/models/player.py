8a76d7c013d999f8019682b09a32210db83b566f
ShinyParadise <shinodise@gmail.com>
2022-10-06 18:34:36 +0300

    models moved

---
+++ app/core/models/player.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Integer, String, Column, ForeignKey
+from sqlalchemy.orm import relationship
+from app.core.database import Base
+
+
+class Player(Base):
+    __tablename__ = "players"
+
+    id = Column(Integer, primary_key=True, index=True)
+    number = Column(Integer, nullable=False, default=1)
+    first_name = Column(String)
+    last_name = Column(String)
+    middle_name = Column(String)
+    team_id = Column(Integer, ForeignKey("teams.id"))
+
+    team = relationship("Team", back_populates="players")
