9113b6098606b414499a4673ca41fd0c34c85678
ShinyParadise <shinodise@gmail.com>
2022-10-15 17:45:34 +0300

    refactored

--- app/core/models/active_players.py
+++ app/core/models/active_players.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Integer, String, Column, ForeignKey
+from sqlalchemy import Integer, Column, ForeignKey
 from sqlalchemy.orm import relationship
+
 from app.core.database import Base
 
 
-class ActivePlayer(Base):
+class ActivePlayers(Base):
     __tablename__ = "active_players"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -13,8 +14,7 @@ class ActivePlayer(Base):
     fourth_player = Column(Integer, ForeignKey("players.id"), nullable=False)
     fifth_player = Column(Integer, ForeignKey("players.id"), nullable=False)
 
-    player_1 = relationship("Player", foreign_keys=[first_player])
-    player_2 = relationship("Player", foreign_keys=[second_player])
-    player_3 = relationship("Player", foreign_keys=[third_player])
-    player_4 = relationship("Player", foreign_keys=[fourth_player])
-    player_5 = relationship("Player", foreign_keys=[fifth_player])
+    active_players = relationship(
+        "Player",
+        foreign_keys=[first_player, second_player, third_player, fourth_player, fifth_player]
+    )
