ab9ed3e78a34830cdd40f3d6266916174d8bfb61
ShinyParadise <shinodise@gmail.com>
2022-10-23 16:51:54 +0300

    added api for possessions

---
+++ app/api/endpoints/possessions.py
@@ -0,0 +1,21 @@
+from fastapi import Depends, APIRouter
+from app.core.crud.possessions_operations import *
+from app.core.schemas.possessions import Possessions as PossessionsSchema
+from app.core.dependencies import get_db
+
+possessions_router = APIRouter(prefix="/possessions", tags=["possessions"])
+
+
+@possessions_router.post("/create", response_model=PossessionsSchema)
+def create(possession: PossessionsCreate, db: Session = Depends(get_db)):
+    return create_possession(db, possession)
+
+
+@possessions_router.get("/get_all", response_model=list[PossessionsSchema])
+def get_all_possessions(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return get_all_possessions(db, skip=skip, limit=limit)
+
+
+@possessions_router.get("/{user_id}", response_model=PossessionsSchema)
+def get_possession_by_id(possession_id: int, db: Session = Depends(get_db)):
+    return get_possession(db=db, possession_id=possession_id)
